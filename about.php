3f699d03ad1656075b86a579f50c2ac71ac05965
Tushar Nangare <51354942+7usharnp@users.noreply.github.com>
2020-07-16 14:36:31 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+   <head>
+        <title></title>
+        <link rel="stylesheet" href="css/styles.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="jumbotron">
+          <h1>7usharTours</h1>
+          <p>Blissfull life....</p>
+</div>
+
+
+
+<section class="my-5">
+                    <div class="text-center">
+                        <h3>About Us </h3>
+                    </div>
+
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <img src="images/4K-Mac-Wallpaper-1.jpg" class="img-fluid aboutimg" >
+                            </div>
+                        
+                            <div class="col-lg-6 col-md-6 col-12">
+                                    <h2>Hello Tourists</h2>
+                                        <p py-5>lorem ipsum dolor sit amet, consectetur adip Lorem 
+                                            ipsum dolor sit amet consectetur, adipisicing elit.
+                                            Rem placeat eum distinctio voluptas aliquid cupiditate, 
+                                            qui laudantium fugiat culpa vel optio illum expedita corrupti.
+                                            Aliquid ratione quisquam sapiente necessitatibus vero.
+                                            ipsum dolor sit amet consectetur, adipisicing elit.
+                                            Rem placeat eum distinctio voluptas aliquid cupiditate, 
+                                            qui laudantium fugiat culpa vel optio illum expedita corrupti.
+                                            
+                                        </p>
+                                    <a href="about.php" class="btn btn-success">Check More</a>
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                   
+                    
+                </section>
+
+</body>
